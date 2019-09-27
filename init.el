d37bb559184479fe312e9b2fdabda3fe41507bae
Dan Wislocki <dwislocki@yieldmo.com>
2019-09-27 17:18:12 -0400

    Add init.el.

---
+++ init.el
@@ -0,0 +1,30 @@
+;;; init.el -- The latest iteration of DW's .emacs / init.el file.
+
+(require 'package)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(inhibit-startup-screen t)
+ '(package-archives
+   (quote
+    (("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa-stable" . "http://stable.melpa.org/packages/"))))
+ '(package-selected-packages
+   (quote
+    (avy deft dockerfile-mode groovy-mode gradle-mode yaml-mode markdown-mode scala-mode sbt-mode flycheck haskell-mode)))
+ '(tool-bar-mode nil))
+
+(package-initialize)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;(add-hook 'after-init-hook #'global-flycheck-mode)
