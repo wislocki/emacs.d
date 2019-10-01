bc4ec6d96533afa5ca2b1810e4175a51f7153ac2
Dan Wislocki <dwislocki@yieldmo.com>
2019-10-01 10:01:15 -0400

    Turn on column-number-mode and bind some more keys.

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
  '(inhibit-startup-screen t)
  '(package-archives
    (quote
@@ -63,6 +64,10 @@
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (require 'zetteldeft)
+
+(global-set-key [f1] 'Info-goto-emacs-command-node)
+
+(global-set-key [f6] 'find-file-other-frame)
 
 (global-set-key [f9] 'zd-new-file)
 
