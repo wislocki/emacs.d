5dc9b0bc683e93d18c89834e7b02ad6cd18ba02e
wislocki <daniel.wislocki@gmail.com>
2021-03-07 00:40:50 -0500

    Change the default font size to 10pts.

--- init.el
+++ init.el
@@ -158,4 +158,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#424242" :foreground "#E0E0E0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "CTDB" :family "Fira Mono")))))
+
+;;(set-face-attribute 'default nil :height 100)
